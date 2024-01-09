da2c46e0baa60dec45c1589aede2844fdbb24845
Eliass-g <eliass.ghauss@gmail.com>
2024-01-09 06:44:41 -0500

    fix CORS authorization

--- backend/server.js
+++ backend/server.js
@@ -33,6 +33,14 @@ app.use(
     keys: ["key1"],
   })
 );
+const cors=require("cors");
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions)) // Use this after the variable declaration
 
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
