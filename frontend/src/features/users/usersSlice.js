6594eb128a615093518f0b02fb25bc9461ada543
Eliass-g <eliass.ghauss@gmail.com>
2024-01-30 15:54:37 -0500

    add functionality for register, logout within redux

--- frontend/src/features/users/usersSlice.js
+++ frontend/src/features/users/usersSlice.js
@@ -4,6 +4,10 @@ axios.defaults.baseURL = "http://localhost:3001";
 
 const initialState = {
   currentUser: {},
+  status: {
+    login: "idle",
+    register: "idle",
+  },
 };
 
 export const usersSlice = createSlice({
@@ -12,30 +16,80 @@ export const usersSlice = createSlice({
   reducers: {},
   extraReducers(builder) {
     builder
-      .addCase(loginUser.pending, () => {
-        console.log("loginUser pending");
+      .addCase(loginUser.pending, (state, action) => {
+        state.status.login = "loading";
       })
       .addCase(loginUser.fulfilled, (state, action) => {
-        console.log("login success");
+        state.status.login = "succeeded";
         state.currentUser = action.payload;
       })
-      .addCase(loginUser.rejected, () => {
-        console.log("loginUser failed");
-    })
+      .addCase(loginUser.rejected, (state, action) => {
+        state.status.login = "failed";
+      })
+      .addCase(registerUser.pending, (state, action) => {
+        state.status.register = "loading";
+      })
+      .addCase(registerUser.fulfilled, (state, action) => {
+        state.status.register = "succeeded";
+        state.status.login = "succeeded";
+        state.currentUser = action.payload;
+      })
+      .addCase(registerUser.rejected, (state, action) => {
+        state.status.register = "failed";
+      })
+      .addCase(logoutUser.pending, (state, action) => {
+        state.status.logout = "loading";
+      })
+      .addCase(logoutUser.fulfilled, (state, action) => {
+        state.status.logout = "succeeded";
+        state.currentUser = {};
+      })
+      .addCase(logoutUser.rejected, (state, action) => {
+        state.status.logout = "failed";
+      });
   },
 });
 
-export const loginUser = createAsyncThunk("users/loginUser", async (userParam) => {
-  const {email, password} = userParam;
-  const result = await axios({
-    url: "/users/login",
+export const loginUser = createAsyncThunk(
+  "users/loginUser",
+  async (userParam) => {
+    const { email, password } = userParam;
+    const response = await axios({
+      url: "/users/login",
+      method: "POST",
+      data: {
+        email,
+        password,
+      },
+    });
+    return response.data;
+  }
+);
+
+export const registerUser = createAsyncThunk(
+  "users/registerUser",
+  async (userParam) => {
+    const { first_name, last_name, email, password } = userParam;
+    const response = await axios({
+      url: "/users/register",
+      method: "POST",
+      data: {
+        first_name,
+        last_name,
+        email,
+        password,
+      },
+    });
+    return response.data;
+  }
+);
+
+export const logoutUser = createAsyncThunk("users/logoutUser", async () => {
+  const response = await axios({
+    url: "/users/logout",
     method: "POST",
-    data: {
-      email,
-      password
-    },
   });
-  return result.data;
+  return response.data;
 });
 
 export const getCurrentUser = (state) => state.users.currentUser;
